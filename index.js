eba4aafbfa42da54a6cb73553794f5892005b8c8
Rodrigo Graúdo <rodrigodh@gmail.com>
2017-05-03 20:47:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ plugin.manifest = (pth, opts) => {
 		const revisionedFile = relPath(file.base, file.path);
 		const originalFile = path.join(path.dirname(revisionedFile), path.basename(file.revOrigPath)).replace(/\\/g, '/');
 
-		manifest[originalFile] = revisionedFile;
+		manifest['/' + originalFile] = '/' + revisionedFile;
 
 		cb();
 	}, function (cb) {
